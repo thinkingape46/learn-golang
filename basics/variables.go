2e3e96599f6fdddd1f3e0dbbfdadb940a7a661a0
jagadeesh <jagadeesh.kumar2091@gmail.com>
2021-09-28 19:59:27 +0530

    initial commit, added variables.go

---
+++ basics/variables.go
@@ -0,0 +1,11 @@
+// variables need to decleared before
+package main
+
+import "fmt"
+
+func main() {
+	var speed int // will be initialized with a value of 0
+	fmt.Println(speed)
+	speed = 100 // speed reassigned to 100
+	fmt.Println(speed)
+}
