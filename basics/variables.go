1c33492cbe225ecfe559cea404f28d5f1084c8c2
jagadeesh <jagadeesh.kumar2091@gmail.com>
2021-09-28 20:00:36 +0530

    updated variables.go

--- basics/variables.go
+++ basics/variables.go
@@ -4,6 +4,7 @@ package main
 import "fmt"
 
 func main() {
+	// fmt.Println(speed)  this won't work because, a variable cannot be called before initialization.
 	var speed int // will be initialized with a value of 0
 	fmt.Println(speed)
 	speed = 100 // speed reassigned to 100
